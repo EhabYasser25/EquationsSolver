6c14f309fa61dd889870cf12109ac83ed432db3e
Random <ehabflash833@gmail.com>
2022-12-19 19:05:18 +0200

    gui

--- Phase1/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.phase1 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.phase1 to javafx.fxml;
-    exports com.example.phase1;
-}
---
+++ Phase1/src/test/Solve.java
@@ -0,0 +1,26 @@
+package test;
+
+import test.GaussElimination.*;
+
+public class Solve {
+	
+	private String method = "Gauss Elimination";
+	private double coef[][];
+	double ans[];
+	
+	public Solve(String method, double[][] coef) {
+		this.method = method;
+		this.coef = coef;
+	}
+	
+	public double[] chooseMethod() {
+		switch(method) {
+			case "Gauss Elimination" : {
+				GaussElimination obj = new GaussElimination(coef, true);
+				ans = obj.solve();
+			}
+		}
+		return ans;
+	}
+
+}
