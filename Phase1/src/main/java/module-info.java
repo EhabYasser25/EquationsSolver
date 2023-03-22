80ae0c8ab14046069424bd6beae2a86cd2035846
Random <ehabflash833@gmail.com>
2023-03-22 13:16:39 +0200

    last commit

--- Phase1/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.phase1 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.desktop;
-
-
-    opens com.example.phase1 to javafx.fxml;
-    exports com.example.phase1;
-}
